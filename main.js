62ad70d9cd3d26eb75d7dde169b4e1d29a585ca1
Jana Haganová <jhaganova@gmail.com>
2022-03-26 16:47:20 +0100

    while

--- main.js
+++ main.js
@@ -6,9 +6,17 @@
 console.log('První příklad - vzestupná řada 0 - 10');
 
 
-for (let i=1; i<=10; i=i+1) {
-    console.log(i);
+// for (let i=0; i<=10; i=i+1) {
+//     console.log(i);
+// }
+
+let  x=0;
+while (x<=10) {
+    console.log(x);
+    x++;   
 }
+
+
 
 
 // Druhý příklad - zkopíruj předchozí příklad
@@ -19,10 +27,15 @@ console.log('Druhý příklad - rozdíl mezi <= a <');
 
 //TADY NAPIS CYKLUS
 
-for (let i=1; i<11; i=i+1) {
-    console.log(i);
-}
+// for (let i=0; i<11; i=i+1) {
+//     console.log(i);
+// }
 
+x = 0
+while (x<11) {
+    console.log(x);
+    x++;  
+}
 
 
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
@@ -30,6 +43,12 @@ console.log('Třetí příklad - sestupná řada 10 - 0');
 
 //TADY NAPIS CYKLUS
 
-for (let i=10; i>=0; i=i-1) {
-    console.log(i);
+// for (let i=10; i>=0; i=i-1) {
+//     console.log(i);
+// }
+
+x=10 
+while (x>=0) {
+    console.log(x);
+    x=x-1;
 }
